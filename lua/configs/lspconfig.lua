bc678d8f672617770a30f1f5727f0de463636924
Fabioomega <fabioms2002@gmail.com>
2025-10-31 17:03:36 -0300

    Added a decent python plugin added gitignore. And fixed my clipboard... Again

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,18 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls" }
+vim.lsp.inlay_hint.enable(true)
+
+local servers = { "html", "cssls", "ty" }
 vim.lsp.enable(servers)
 
--- read :h vim.lsp.config for changing options of lsp servers 
+-- read :h vim.lsp.config for changing options of lsp servers
+
+local util = require("lspconfig.util")
+local based_cfg = vim.lsp.config.ty
+
+vim.lsp.config("ty", {
+	root_dir = function(bufnr, on_dir)
+		local workdir = util.root_pattern(unpack(based_cfg.root_markers))(bufnr) or vim.uv.cwd()
+		on_dir(workdir)
+	end,
+})
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,50 +1,48 @@
 return {
-  {
-    "stevearc/conform.nvim",
-    event = { "BufWritePre" },
-    cmd = { "ConformInfo" },
-    opts = require "configs.conform",
-  },
+	{
+		"stevearc/conform.nvim",
+		event = { "BufWritePre" },
+		cmd = { "ConformInfo" },
+		opts = require("configs.conform"),
+	},
+	{
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("configs.lspconfig")
+		end,
+	},
+	{
+		"mrcjkb/rustaceanvim",
+		lazy = false, -- This plugin is already lazy
+	},
+	{
+		"mg979/vim-visual-multi",
+		lazy = false,
+	},
+	{
+		"tpope/vim-fugitive",
+		cmd = {
+			"Git",
+			"Gdiffsplit",
+			"Gread",
+			"Gwrite",
+			"Ggrep",
+			"GMove",
+			"GDelete",
+			"GBrowse",
+		},
+	},
 
-  -- These are some examples, uncomment them if you want to see them work!
-  {
-    "neovim/nvim-lspconfig",
-    config = function()
-      require "configs.lspconfig"
-    end,
-  },
-  {
-    "mrcjkb/rustaceanvim",
-    lazy = false, -- This plugin is already lazy
-  },
-  {
-    "mg979/vim-visual-multi",
-    lazy = false,
-  },
-  {
-    "tpope/vim-fugitive",
-    cmd = {
-      "Git",
-      "Gdiffsplit",
-      "Gread",
-      "Gwrite",
-      "Ggrep",
-      "GMove",
-      "GDelete",
-      "GBrowse",
-    },
-  },
+	-- test new blink
+	-- { import = "nvchad.blink.lazyspec" },
 
-  -- test new blink
-  -- { import = "nvchad.blink.lazyspec" },
-
-  -- {
-  -- 	"nvim-treesitter/nvim-treesitter",
-  -- 	opts = {
-  -- 		ensure_installed = {
-  -- 			"vim", "lua", "vimdoc",
-  --      "html", "css"
-  -- 		},
-  -- 	},
-  -- },
+	-- {
+	-- 	"nvim-treesitter/nvim-treesitter",
+	-- 	opts = {
+	-- 		ensure_installed = {
+	-- 			"vim", "lua", "vimdoc",
+	--      "html", "css"
+	-- 		},
+	-- 	},
+	-- },
 }
