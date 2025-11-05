93e819c886ba96a4d8db58cecc7451957172030e
Fabioomega <fabioms2002@gmail.com>
2025-11-05 00:56:22 -0300

    Made the root_dir fallback dynamic

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -6,13 +6,19 @@ local servers = { "html", "cssls", "ty", "clangd" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers
-
 local util = require("lspconfig.util")
-local based_cfg = vim.lsp.config.ty
 
-vim.lsp.config("ty", {
-	root_dir = function(bufnr, on_dir)
-		local workdir = util.root_pattern(unpack(based_cfg.root_markers))(bufnr) or vim.uv.cwd()
-		on_dir(workdir)
-	end,
-})
+local use_root_fallback = { "ty", "clangd" }
+
+for i = 1, #use_root_fallback do
+	local server_name = use_root_fallback[i]
+
+	local based_cfg = vim.lsp.config[server_name]
+
+	vim.lsp.config(server_name, {
+		root_dir = function(bufnr, on_dir)
+			local workdir = util.root_pattern(unpack(based_cfg.root_markers))(bufnr) or vim.uv.cwd()
+			on_dir(workdir)
+		end,
+	})
+end
