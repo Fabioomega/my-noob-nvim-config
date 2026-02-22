5bc8828e62dbc0ea67d67bef1a9f6bfa824f62ae
Fabioomega <fabioms2002@gmail.com>
2026-02-22 13:07:50 -0300

    Added folding support and some other goodies

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,58 +1,68 @@
 return {
-	{
-		"stevearc/conform.nvim",
-		event = { "BufWritePre" },
-		cmd = { "ConformInfo" },
-		opts = require("configs.conform"),
-	},
-	{
-		"neovim/nvim-lspconfig",
-		config = function()
-			require("configs.lspconfig")
-		end,
-	},
-	{
-		"mrcjkb/rustaceanvim",
-		lazy = false, -- This plugin is already lazy
-	},
-	{
-		"mg979/vim-visual-multi",
-		lazy = false,
-	},
-	{
-		"tpope/vim-fugitive",
-		cmd = {
-			"Git",
-			"Gdiffsplit",
-			"Gread",
-			"Gwrite",
-			"Ggrep",
-			"GMove",
-			"GDelete",
-			"GBrowse",
-		},
-	},
-	{
-		"nvim-tree/nvim-tree.lua",
-		cmd = { "NvimTreeToggle", "NvimTreeFocus" },
-		opts = function()
-			local opts = require("nvchad.configs.nvimtree")
+  {
+    "stevearc/conform.nvim",
+    event = { "BufWritePre" },
+    cmd = { "ConformInfo" },
+    opts = require "configs.conform",
+  },
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "mrcjkb/rustaceanvim",
+    lazy = false, -- This plugin is already lazy
+  },
+  {
+    "mg979/vim-visual-multi",
+    lazy = false,
+    init = function()
+      vim.g.VM_maps = {
+        ["Add Cursor Down"] = "<C-Down>",
+        ["Add Cursor Up"] = "<C-Up>",
+        ["Add Cursor Left"] = "<C-Left>",
+        ["Add Cursor Right"] = "<C-Right>",
+        ["Select l"] = "",
+        ["Select h"] = "",
+      }
+    end,
+  },
+  {
+    "tpope/vim-fugitive",
+    cmd = {
+      "Git",
+      "Gdiffsplit",
+      "Gread",
+      "Gwrite",
+      "Ggrep",
+      "GMove",
+      "GDelete",
+      "GBrowse",
+    },
+  },
+  {
+    "nvim-tree/nvim-tree.lua",
+    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    opts = function()
+      local opts = require "nvchad.configs.nvimtree"
 
-			opts.filters.git_ignored = false
+      opts.filters.git_ignored = false
 
-			return opts
-		end,
-	},
-	-- test new blink
-	-- { import = "nvchad.blink.lazyspec" },
+      return opts
+    end,
+  },
+  -- test new blink
+  -- { import = "nvchad.blink.lazyspec" },
 
-	-- {
-	-- 	"nvim-treesitter/nvim-treesitter",
-	-- 	opts = {
-	-- 		ensure_installed = {
-	-- 			"vim", "lua", "vimdoc",
-	--      "html", "css"
-	-- 		},
-	-- 	},
-	-- },
+  -- {
+  -- 	"nvim-treesitter/nvim-treesitter",
+  -- 	opts = {
+  -- 		ensure_installed = {
+  -- 			"vim", "lua", "vimdoc",
+  --      "html", "css"
+  -- 		},
+  -- 	},
+  -- },
 }
