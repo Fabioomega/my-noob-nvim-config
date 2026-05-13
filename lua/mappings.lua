05bc26572790a4ecbbefb929755c9cd2e952f8f0
Fabioomega <fabioms2002@gmail.com>
2026-05-13 17:36:15 -0300

    Toggle display hints

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -1,4 +1,4 @@
-require "nvchad.mappings"
+require("nvchad.mappings")
 
 local map = vim.keymap.set
 
@@ -6,35 +6,35 @@ map("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
 
 -- Opens telescope with diagnostics set to error
 map("n", "<leader>E", function()
-  require("telescope.builtin").diagnostics {
-    severity = vim.diagnostic.severity.ERROR,
-  }
+	require("telescope.builtin").diagnostics({
+		severity = vim.diagnostic.severity.ERROR,
+	})
 end, { desc = "Show buffer errors" })
 
 -- Adds format behavior and leaves visual mode when format range was performed fudge
 map("", "<leader>F", function()
-  require("conform").format({ async = true }, function(err)
-    if not err then
-      local mode = vim.api.nvim_get_mode().mode
-      if vim.startswith(string.lower(mode), "v") then
-        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
-      end
-    end
-  end)
+	require("conform").format({ async = true }, function(err)
+		if not err then
+			local mode = vim.api.nvim_get_mode().mode
+			if vim.startswith(string.lower(mode), "v") then
+				vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+			end
+		end
+	end)
 end, { desc = "Format code" })
 
 -- Opens a LSP diagnostics locklist with workspace-wide errors
 map("n", "<leader>dE", function()
-  vim.diagnostic.setqflist {
-    severity = vim.diagnostic.severity.ERROR,
-  }
+	vim.diagnostic.setqflist({
+		severity = vim.diagnostic.severity.ERROR,
+	})
 end, { desc = "LSP diagnostic locklist (workspace-wide errors)" })
 
 -- Adds find the references of a symbol
 map("n", "<leader>fR", function()
-  require("telescope.builtin").lsp_references {
-    reuse_win = true,
-  }
+	require("telescope.builtin").lsp_references({
+		reuse_win = true,
+	})
 end, { desc = "telescope find references" })
 
 -- Shows the contents of all registers
@@ -45,3 +45,8 @@ map({ "n", "x" }, "<leader>ca", require("fastaction").code_action, { desc = "Dis
 map("n", ";", ":", { desc = "CMD enter command mode" })
 -- When <ESC> is pressed exit insert mode in the terminal
 map("i", "jk", "<ESC>")
+
+-- Toggle display hints
+map("n", "<leader>dh", function()
+	vim.lsp.inlay_hint.enable(vim.lsp.inlay_hint.is_enabled())
+end, { desc = "toggle display hints" })
